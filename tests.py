a3213aba38ffab69df1c971a87565747c61f41fe
softcookiepp <softcookiepp@mailfence.com>
2025-04-03 13:13:24 -0700

    reduced testing timesteps

--- tests.py
+++ tests.py
@@ -399,10 +399,10 @@ def test_stable_diffusion_pipeline():
 	from tiny_hf.diffusers.pipelines import StableDiffusionPipeline as tg_class
 	from diffusers.pipelines import StableDiffusionPipeline as hf_class
 	
-	hf_module = hf_class.from_pretrained("stablediffusionapi/anything-v5", use_safetensors = True, device = "cuda")
-	tg_module = tg_class.from_pretrained("stablediffusionapi/anything-v5", use_safetensors = True, device = "cuda")
+	hf_module = hf_class.from_pretrained("stablediffusionapi/anything-v5", use_safetensors = True)
+	tg_module = tg_class.from_pretrained("stablediffusionapi/anything-v5", use_safetensors = True)
 	
-	test_hf_reimplementation([], {"prompt": "a fluffy bunny"}, hf_module, "__call__", tg_module, "__call__")
+	test_hf_reimplementation([], {"prompt": "a fluffy bunny", "timesteps": 3}, hf_module, "__call__", tg_module, "__call__")
 	
 
 @tinygrad.Tensor.test()
