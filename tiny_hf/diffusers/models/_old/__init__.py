7f5ecc07ffce96dbb87267ab5453b792afb4aaa5
softcookiepp <softcookiepp@mailfence.com>
2025-04-03 10:27:52 -0700

    did it work

---
+++ tiny_hf/diffusers/models/_old/__init__.py
@@ -0,0 +1,8 @@
+
+from . import autoencoders#unet, autoencoders
+from .unets import *
+from .autoencoders import *#AutoencoderKL
+from .embeddings import *
+from .adapter import *
+from .controlnets import *
+from .transformers import *
