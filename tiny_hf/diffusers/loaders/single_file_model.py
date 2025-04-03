691d1936fa43afd4b13d0eb7451683379f459747
softcookiepp <softcookiepp@mailfence.com>
2025-04-03 10:23:35 -0700

    beep boop

--- tiny_hf/diffusers/loaders/single_file_model.py
+++ tiny_hf/diffusers/loaders/single_file_model.py
@@ -140,7 +140,8 @@ SINGLE_FILE_LOADABLE_CLASSES = {
 
 
 def _get_single_file_loadable_mapping_class(cls):
-	diffusers_module = importlib.import_module(__name__.split(".")[0])
+	diffusers_module = importlib.import_module(__name__.split(".")[1])
+	input(diffusers_module)
 	for loadable_class_str in SINGLE_FILE_LOADABLE_CLASSES:
 		loadable_class = getattr(diffusers_module, loadable_class_str)
 
