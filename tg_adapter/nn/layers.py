b8dfd5c03a9b5d7ea1c52a95af28dc01374ec005
softcookiepp <softcookiepp@mailfence.com>
2025-04-07 17:15:33 -0700

    just look at the code silly

--- tg_adapter/nn/layers.py
+++ tg_adapter/nn/layers.py
@@ -1,7 +1,9 @@
 from .module import Module
 from tinygrad import Tensor
+from tinygrad.helpers import make_tuple, prod
 from ..tensor import _convert_base as _cb
 from ..tensor import _disinherit
+import math
 
 class AvgPool2d(Module):
 	def __init__(self, kernel_size, stride=None, padding=0,
@@ -82,5 +84,62 @@ class Upsample(Module):
 	
 	def forward(self, *args, **kwargs):
 		raise NotImplementedError
+
+class Conv2d:
+
+  def __init__(self, in_channels:int, out_channels:int, kernel_size:int|tuple[int, ...], stride=1, padding:int|tuple[int, ...]|str=0,
+               dilation=1, groups=1, bias=True):
+    self.kernel_size = make_tuple(kernel_size, 2)
+    if isinstance(padding, str):
+      if padding.lower() != 'same': raise ValueError(f"Invalid padding string {padding!r}, only 'same' is supported")
+      if stride != 1: raise ValueError("padding='same' is not supported for strided convolutions")
+      pad = [(d*(k-1)//2, d*(k-1) - d*(k-1)//2) for d,k in zip(make_tuple(dilation, len(self.kernel_size)), self.kernel_size[::-1])]
+      padding = tuple(flatten(pad))
+    self.stride, self.dilation, self.groups, self.padding = stride, dilation, groups, padding
+    scale = 1 / math.sqrt(in_channels * prod(self.kernel_size))
+    self.weight = Tensor.uniform(out_channels, in_channels//groups, *self.kernel_size, low=-scale, high=scale)
+    self.bias: Tensor|None = Tensor.uniform(out_channels, low=-scale, high=scale) if bias else None
+
+  def __call__(self, x:Tensor) -> Tensor: return x.conv2d(self.weight, self.bias, self.groups, self.stride, self.dilation, self.padding)
 		
-class Conv1d
+class ConvNd(Module):
+	def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+			padding=0, dilation=1, groups=1, bias=True,
+			padding_mode='zeros', device=None, dtype=None, dim = None):
+		
+		# must have dimensionality
+		assert not dim is None
+		if isinstance(padding, str):
+			assert padding in ["valid", "same"]
+		self.kernel_size = make_tuple(kernel_size, dim)
+		self.stride, self.dilation, self.groups, self.padding = stride, dilation, groups, padding
+		scale = 1 / math.sqrt(in_channels * prod(self.kernel_size))
+		
+		self.weight = _cb(self.weight = Tensor.uniform(out_channels, in_channels//groups, *self.kernel_size, low=-scale, high=scale) )
+		
+		self.bias = None
+		if bias:
+			self.bias = _cb(tinygrad.Tensor.uniform(Tensor.uniform(out_channels, low=-scale, high=scale) )
+	
+	def forward(self, x):
+		return x.conv2d(self.weight, self.bias, self.groups, self.stride, self.dilation, self.padding)
+
+# ugh, I forgot that torch is going to expect this crap as a type :c
+
+def Conv1d(in_channels, out_channels, kernel_size, stride=1,
+			padding=0, dilation=1, groups=1, bias=True,
+			padding_mode='zeros', device=None, dtype=None, dim = None):
+	return ConvNd(in_channels, out_channels, kernel_size, stride,
+			padding, dilation, groups, bias, padding_mode, device, dtype, dim = 1)
+	
+def Conv2d(in_channels, out_channels, kernel_size, stride=1,
+			padding=0, dilation=1, groups=1, bias=True,
+			padding_mode='zeros', device=None, dtype=None, dim = None):
+	return ConvNd(in_channels, out_channels, kernel_size, stride,
+			padding, dilation, groups, bias, padding_mode, device, dtype, dim = 2)
+
+def Conv3d(in_channels, out_channels, kernel_size, stride=1,
+			padding=0, dilation=1, groups=1, bias=True,
+			padding_mode='zeros', device=None, dtype=None, dim = None):
+	return ConvNd(in_channels, out_channels, kernel_size, stride,
+			padding, dilation, groups, bias, padding_mode, device, dtype, dim = 3)
