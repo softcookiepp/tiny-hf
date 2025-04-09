fd5b282cb39bc8cdf9d266475b52d0122530abdd
softcookiepp <softcookiepp@mailfence.com>
2025-04-09 11:36:04 -0700

    re-added disinherit

--- tg_adapter/tensor.py
+++ tg_adapter/tensor.py
@@ -76,3 +76,42 @@ def _convert_base(inp):
 		else:
 			# inp is a primitive type
 			return inp
+
+def _disinherit(*inp):
+	if len(inp) == 1:
+		inp = inp[0]
+	if isinstance(inp, AdapterTensor):
+		t = tinygrad.Tensor(None)
+		t.lazydata = inp.lazydata
+		assert not "CUDA" in t.device
+		return t
+	if isinstance(inp, tinygrad.Tensor):
+		# do nothing
+		assert not "CUDA" in inp.device
+		return inp
+	elif isinstance(inp, list) or isinstance(inp, tuple):
+		new = []
+		for item in inp:
+			new.append(_disinherit(item) )
+		if isinstance(inp, tuple):
+			new = tuple(new)
+		for elem in new:
+			assert not isinstance(elem, AdapterTensor)
+			
+		return new
+	elif isinstance(inp, dict):
+		for k, v in inp.items():
+			inp[k] = _disinherit(v)
+		return inp
+	else:
+		if hasattr(inp, "__dict__"):
+			# treat as dictionary hehe
+			new_dict = _disinherit(inp.__dict__)
+			inp.__dict__.update(new_dict)
+			return inp
+		else:
+			# inp is a primitive type
+			return inp
+
+	def _getitem(self, key):
+		raise NotImplementedError
