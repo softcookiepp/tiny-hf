c52e4c60b0a9c8851904b137fe72328cfe879b58
softcookiepp <softcookiepp@mailfence.com>
2025-04-09 14:55:20 -0700

    weeeeeeeeeeeeeeeee

--- tiny_hf/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ tiny_hf/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -422,6 +422,7 @@ class StableDiffusionPipeline(
 				attention_mask = None
 
 			if clip_skip is None:
+				print(type(self.tokenizer) )
 				print(type(text_input_ids) )
 				input()
 				prompt_embeds = self.text_encoder(text_input_ids.to(device), attention_mask=attention_mask)
