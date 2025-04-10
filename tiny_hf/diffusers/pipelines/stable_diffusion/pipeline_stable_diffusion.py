f0d897142c5cd801ad8da6a2a0caab0fd01750ac
softcookiepp <softcookiepp@mailfence.com>
2025-04-10 11:08:54 -0700

    debug

--- tiny_hf/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ tiny_hf/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -1088,7 +1088,6 @@ class StableDiffusionPipeline(
 			image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False, generator=generator)[
 				0
 			]
-			print(image)
 			image, has_nsfw_concept = self.run_safety_checker(image, device, prompt_embeds.dtype)
 		else:
 			image = latents
