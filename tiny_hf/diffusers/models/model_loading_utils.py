9688e7efca2968ead51535fd7924f5027de0cc53
softcookiepp <softcookiepp@mailfence.com>
2025-05-15 14:21:12 -0700

    trying to fix the state dict loader

--- tiny_hf/diffusers/models/model_loading_utils.py
+++ tiny_hf/diffusers/models/model_loading_utils.py
@@ -333,6 +333,7 @@ def _load_state_dict_into_model(
 		local_metadata["assign_to_params_buffers"] = assign_to_params_buffers
 		if assign_to_params_buffers and not is_torch_version(">=", "2.1"):
 			logger.info("You need to have torch>=2.1 in order to load the model with assign_to_params_buffers=True")
+		input(prefix)
 		args = (state_dict, prefix, local_metadata, True, [], [], error_msgs)
 		module._load_from_state_dict(*args)
 
