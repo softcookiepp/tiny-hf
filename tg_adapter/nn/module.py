deb6226bdbe166af85fc23a80dbfc64a05a1e16b
softcookiepp <softcookiepp@mailfence.com>
2025-05-16 11:55:25 -0700

    hmmmmm

--- tg_adapter/nn/module.py
+++ tg_adapter/nn/module.py
@@ -229,7 +229,7 @@ class Module:
 			k = k + "._tg"
 			#input(k)
 			new_state_dict[k] = v
-		input(new_state_dict.keys() )
+		#input(new_state_dict.keys() )
 		
 		tinygrad.nn.state.load_state_dict(self, new_state_dict, strict = strict, verbose = True)
 		#_cb(self)
