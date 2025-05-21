25562714c6ce2064ee6c162071c847e01f15b1d6
softcookiepp <softcookiepp@mailfence.com>
2025-05-21 11:00:47 -0700

    Investigating scheduler NaN causes

--- tiny_hf/diffusers/schedulers/scheduling_ddim.py
+++ tiny_hf/diffusers/schedulers/scheduling_ddim.py
@@ -413,6 +413,7 @@ class DDIMScheduler(SchedulerMixin, ConfigMixin):
 		# "predicted x_0" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
 		if self.config.prediction_type == "epsilon":
 			pred_original_sample = (sample - beta_prod_t ** (0.5) * model_output) / alpha_prod_t ** (0.5)
+			print(pred_original_sample.numpy() )
 			input(np.isnan(np.sum(pred_original_sample.numpy() ) ) )
 			pred_epsilon = model_output
 		elif self.config.prediction_type == "sample":
