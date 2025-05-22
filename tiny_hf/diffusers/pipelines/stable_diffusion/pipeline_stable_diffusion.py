233fdc08165cfc9b14df15c14aeae22d9caa3229
softcookiepp <softcookiepp@mailfence.com>
2025-05-22 14:34:38 -0700

    dumb crap

--- tiny_hf/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ tiny_hf/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -1062,7 +1062,6 @@ class StableDiffusionPipeline(
 					noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=self.guidance_rescale)
 
 				# compute the previous noisy sample x_t -> x_t-1
-				print("extra_step_kwargs:", extra_step_kwargs)
 				latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
 				if callback_on_step_end is not None:
 					callback_kwargs = {}
