71ebc3aa8664d9cb74f8dbcdb4b6e40a54e1c9ee
softcookiepp <softcookiepp@mailfence.com>
2025-05-23 14:35:19 -0700

    pp

--- testing_rewrites.py
+++ testing_rewrites.py
@@ -340,7 +340,7 @@ def sd_pipeline_call(
 
 			# compute the previous noisy sample x_t -> x_t-1
 			latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
-			
+			return latents
 			if callback_on_step_end is not None:
 				callback_kwargs = {}
 				for k in callback_on_step_end_tensor_inputs:
