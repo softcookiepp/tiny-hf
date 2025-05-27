2127e95f37984c025f5a5ebcc95816aa93b6eab2
softcookiepp <softcookiepp@mailfence.com>
2025-05-27 10:02:17 -0700

    gey

--- tests.py
+++ tests.py
@@ -329,7 +329,7 @@ def main():
 	#test_clip_text_model()
 	#test_unet_2d()
 	#test_unet_2d_condition()
-	test_unary()
+	test_unary(None, None)
 	test_interpolate()
 	test_cat()
 	test_ddim_scheduler()
