5f0393f7069d790f7783c416822d14bd5da674aa
softcookiepp <softcookiepp@mailfence.com>
2025-05-27 11:00:46 -0700

    gey

--- tests.py
+++ tests.py
@@ -268,11 +268,17 @@ def test_stable_diffusion_pipeline():
 	hf_module = hf_class.from_pretrained("stablediffusionapi/anything-v5", use_safetensors = True, requires_safety_checker = False, scheduler = hf_scheduler, safety_checker = None)
 	tg_module = tg_class.from_pretrained("stablediffusionapi/anything-v5", use_safetensors = True, requires_safety_checker = False, scheduler = tg_scheduler, safety_checker = None)
 	
+	
+	
 	# ensure there is no difference in state dict
 	compare_state_dicts(hf_module.unet, tg_module.unet)
+	compare_state_dicts(hf_module.vae, tg_module.vae)
 	
 	# oh wait, i realized its impossible for them to have the same output image if the initial latents are not the same
 	latents = make_test_data(1, 4, 64, 64)
+	
+	# test the vae too
+	test_hf_reimplementation([latents], {}, hf_module.vae, "decode", tg_module.vae, "decode")
 	
 	# test the unet
 	# Ok, so the unet conditioner thingy is semi-broken, and I really don't know why...
