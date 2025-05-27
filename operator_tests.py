b861c60691091cb22b2e2af00636b104957f7df4
softcookiepp <softcookiepp@mailfence.com>
2025-05-27 09:42:11 -0700

    gey

--- operator_tests.py
+++ operator_tests.py
@@ -10,7 +10,7 @@ def test_cumprod():
 	import_non_recursive()
 	from tg_adapter import F as tinyF
 	inp = np.arange(5*4).reshape(5, 4).astype(np.float32)
-	test_function( (inp, 0), torch.cumprod, tinyF.cumprod)
+	test_function( (inp, 0), {}, torch.cumprod, tinyF.cumprod)
 
 
 def test_cat():
