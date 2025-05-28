d1e7d3da1d7843997c8ce06406be30daab70faa0
softcookiepp <softcookiepp@mailfence.com>
2025-05-28 12:48:32 -0700

    poopnis

--- tiny_hf/diffusers/models/activations.py
+++ tiny_hf/diffusers/models/activations.py
@@ -111,8 +111,6 @@ class GEGLU(nn.Module):
         return F.gelu(gate)
 
     def forward(self, hidden_states, *args, **kwargs):
-        print(args, kwargs)
-        input()
         if len(args) > 0 or kwargs.get("scale", None) is not None:
             deprecation_message = "The `scale` argument is deprecated and will be ignored. Please remove it, as passing it will raise an error in the future. `scale` should directly be passed while calling the underlying pipeline component i.e., via `cross_attention_kwargs`."
             deprecate("scale", "1.0.0", deprecation_message)
