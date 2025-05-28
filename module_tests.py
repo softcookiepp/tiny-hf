b453d75786b9e3e60c7a34736e564365b59d8806
softcookiepp <softcookiepp@mailfence.com>
2025-05-28 11:23:02 -0700

    does the test data just need to be bigger?

--- module_tests.py
+++ module_tests.py
@@ -7,10 +7,10 @@ def _test_geglu(use_bias = True):
 	from tiny_hf.diffusers.models.activations import GEGLU as tg_class
 	from diffusers.models.activations import GEGLU as hf_class
 	
-	hf_module = hf_class(8, 4, use_bias)
-	tg_module = tg_class(8, 4, use_bias)
+	hf_module = hf_class(4096, 2048, use_bias)
+	tg_module = tg_class(4096, 2048, use_bias)
 	copy_state_dict(hf_module, tg_module)
-	a = make_test_data(128, 8)
+	a = make_test_data(1, 4096)
 	test_hf_reimplementation([a], {}, hf_module, "__call__", tg_module, "__call__")
 
 def test_geglu():
