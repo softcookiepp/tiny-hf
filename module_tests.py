391da77ef1403f65f6ecc123c7513c220942dd65
softcookiepp <softcookiepp@mailfence.com>
2025-05-28 10:06:01 -0700

    changed crap

--- module_tests.py
+++ module_tests.py
@@ -10,7 +10,7 @@ def _test_geglu(use_bias = True):
 	hf_module = hf_class(8, 4, use_bias)
 	tg_module = tg_class(8, 4, use_bias)
 	copy_state_dict(hf_module, tg_module)
-	a = np.arange(64*8).reshape(64, 8).astype(np.float32)
+	a = np.arange(64*8).reshape(64, 8).astype(np.float32)*100
 	test_hf_reimplementation([a], {}, hf_module, "__call__", tg_module, "__call__")
 
 def test_geglu():
