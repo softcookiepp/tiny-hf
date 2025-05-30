4cc6a7607e8ea0e93998e0eea86c73c2f28b6266
softcookiepp <softcookiepp@mailfence.com>
2025-05-30 14:54:25 -0700

    ok maybe its not rsub, its probably something else!

--- testing_rewrites.py
+++ testing_rewrites.py
@@ -397,6 +397,8 @@ def sd_pipeline_call(
 def _get_variance(self, torch, timestep, prev_timestep):
 	alpha_prod_t = self.alphas_cumprod[timestep]
 	alpha_prod_t_prev = self.alphas_cumprod[prev_timestep] if prev_timestep >= 0 else self.final_alpha_cumprod
+	print(prev_timestep)
+	input(prev_timestep >= 0)
 	beta_prod_t = 1 - alpha_prod_t
 	beta_prod_t_prev = 1 - alpha_prod_t_prev
 
