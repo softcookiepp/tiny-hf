75a7c3c232ce828b19c85fc8fa0ac7de91807f52
softcookiepp <softcookiepp@mailfence.com>
2025-05-30 11:03:27 -0700

    i dont even know buddy

--- testing_rewrites.py
+++ testing_rewrites.py
@@ -340,7 +340,9 @@ def sd_pipeline_call(
 				noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=self.guidance_rescale)
 
 			# compute the previous noisy sample x_t -> x_t-1
+			#latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
 			latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
+			print(type(latents) )
 			
 			if i == self._num_timesteps - 1:
 				print(timesteps)
@@ -390,3 +392,97 @@ def sd_pipeline_call(
 
 	out = StableDiffusionPipelineOutput(images=image, nsfw_content_detected=has_nsfw_concept)
 	return out
+	
+def ddim_step(self,
+		torch,
+		model_output,
+		timestep,
+		sample: torch.Tensor,
+		eta: float = 0.0,
+		use_clipped_model_output: bool = False,
+		generator=None,
+		variance_noise: Optional[torch.Tensor] = None,
+		return_dict: bool = True,):
+			
+	if self.num_inference_steps is None:
+		raise ValueError(
+			"Number of inference steps is 'None', you need to run 'set_timesteps' after creating the scheduler"
+		)
+
+	# See formulas (12) and (16) of DDIM paper https://arxiv.org/pdf/2010.02502.pdf
+	# Ideally, read DDIM paper in-detail understanding
+
+	# Notation (<variable name> -> <name in paper>
+	# - pred_noise_t -> e_theta(x_t, t)
+	# - pred_original_sample -> f_theta(x_t, t) or x_0
+	# - std_dev_t -> sigma_t
+	# - eta -> η
+	# - pred_sample_direction -> "direction pointing to x_t"
+	# - pred_prev_sample -> "x_t-1"
+
+	# 1. get previous step value (=t-1)
+	prev_timestep = timestep - self.config.num_train_timesteps // self.num_inference_steps
+
+	# 2. compute alphas, betas
+	alpha_prod_t = self.alphas_cumprod[timestep]
+	alpha_prod_t_prev = self.alphas_cumprod[prev_timestep] if prev_timestep >= 0 else self.final_alpha_cumprod
+
+	beta_prod_t = 1 - alpha_prod_t
+
+	# 3. compute predicted original sample from predicted noise also called
+	# "predicted x_0" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+	if self.config.prediction_type == "epsilon":
+		pred_original_sample = (sample - beta_prod_t ** (0.5) * model_output) / alpha_prod_t ** (0.5)
+		pred_epsilon = model_output
+	elif self.config.prediction_type == "sample":
+		pred_original_sample = model_output
+		pred_epsilon = (sample - alpha_prod_t ** (0.5) * pred_original_sample) / beta_prod_t ** (0.5)
+	elif self.config.prediction_type == "v_prediction":
+		pred_original_sample = (alpha_prod_t**0.5) * sample - (beta_prod_t**0.5) * model_output
+		pred_epsilon = (alpha_prod_t**0.5) * model_output + (beta_prod_t**0.5) * sample
+	else:
+		raise ValueError(
+			f"prediction_type given as {self.config.prediction_type} must be one of `epsilon`, `sample`, or"
+			" `v_prediction`"
+		)
+
+	# 4. Clip or threshold "predicted x_0"
+	if self.config.thresholding:
+		pred_original_sample = self._threshold_sample(pred_original_sample)
+	elif self.config.clip_sample:
+		pred_original_sample = pred_original_sample.clamp(
+			-self.config.clip_sample_range, self.config.clip_sample_range
+		)
+
+	# 5. compute variance: "sigma_t(η)" -> see formula (16)
+	# σ_t = sqrt((1 − α_t−1)/(1 − α_t)) * sqrt(1 − α_t/α_t−1)
+	variance = self._get_variance(timestep, prev_timestep)
+	std_dev_t = eta * variance ** (0.5)
+
+	if use_clipped_model_output:
+		# the pred_epsilon is always re-derived from the clipped x_0 in Glide
+		pred_epsilon = (sample - alpha_prod_t ** (0.5) * pred_original_sample) / beta_prod_t ** (0.5)
+	# 6. compute "direction pointing to x_t" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+	pred_sample_direction = (1 - alpha_prod_t_prev - std_dev_t**2) ** (0.5) * pred_epsilon
+	# 7. compute x_t without "random noise" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+	prev_sample = alpha_prod_t_prev ** (0.5) * pred_original_sample + pred_sample_direction
+	if eta > 0:
+		if variance_noise is not None and generator is not None:
+			raise ValueError(
+				"Cannot pass both generator and variance_noise. Please make sure that either `generator` or"
+				" `variance_noise` stays `None`."
+			)
+
+		if variance_noise is None:
+			variance_noise = randn_tensor(
+				model_output.shape, generator=generator, device=model_output.device, dtype=model_output.dtype
+			)
+		variance = std_dev_t * variance_noise
+		prev_sample = prev_sample + variance
+	if not return_dict:
+		return (
+			prev_sample,
+			pred_original_sample,
+		)
+	raise NotImplementedError
+	return DDIMSchedulerOutput(prev_sample=prev_sample, pred_original_sample=pred_original_sample)
