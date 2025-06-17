0b1ba01c601e829285cf33365692adf288c0993e
softcookiepp <softcookiepp@mailfence.com>
2025-06-17 13:39:48 -0700

    disabled accelerate available

--- tiny_hf/transformers/modeling_utils.py
+++ tiny_hf/transformers/modeling_utils.py
@@ -800,8 +800,6 @@ def _load_state_dict_into_meta_model(
 	from `shard_file`, which is the actual state dict file on disk.
 	This function takes care of correctly casting dtypes, devices, and sharding tensors in case of tensor parallelism.
 	"""
-	print(list(model.parameters())[0])
-	input(list(state_dict.items() )[0][1])
 	tensor_device = "cpu"
 	if device_map is not None and device_map.get("", None) is not None:
 		tensor_device = device_map[""].index if isinstance(device_map[""], torch.device) else device_map[""]
@@ -815,7 +813,7 @@ def _load_state_dict_into_meta_model(
 	if is_meta_state_dict:
 		# This part seems dependent on torch
 		file_pointer = safe_open(shard_file, framework="pt", device=tensor_device)
-	input(file_pointer)
+	
 	for param_name, empty_param in state_dict.items():
 		if param_name not in expected_keys:
 			continue
