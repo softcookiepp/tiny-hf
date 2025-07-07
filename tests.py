663497c682e1ef576089439c0d254532a8b00df2
softcookiepp <softcookiepp@mailfence.com>
2025-07-07 00:44:00 -0700

    saved

--- tests.py
+++ tests.py
@@ -531,10 +531,10 @@ def test_quantized_weights():
 	tg_transformer = tg_FluxTransformer2DModel.from_single_file(ckpt_path,
 		quantization_config = tg_GGUFQuantizationConfig(compute_dtype = tg_adapter.float32),
 		torch_dtype = tg_adapter.float32)
-	
+	input(tg_transformer)
 	tg_pipe = tg_FluxPipeline.from_pretrained(
 		"black-forest-labs/FLUX.1-dev",
-		transformer=hf_transformer,
+		transformer=tg_transformer,
 		torch_dtype=tg_adapter.float32,
 	)
 	
