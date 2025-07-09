49845760d07524146bfe8be7ec81e5d1d6e28646
softcookiepp <softcookiepp@mailfence.com>
2025-07-09 08:37:41 -0700

    debugging

--- tiny_hf/transformers/utils/hub.py
+++ tiny_hf/transformers/utils/hub.py
@@ -421,7 +421,7 @@ def cached_files(
     try:
         if len(full_filenames) == 1:
             # This is slightly better for only 1 file
-            print(path_or_repo_id)
+            print({"path_or_repo_id": path_or_repo_id})
             input(filenames)
             hf_hub_download(
                 path_or_repo_id,
