7220d76c81d569c82b11627397adc5a2654d30d4
softcookiepp <softcookiepp@mailfence.com>
2025-07-10 08:22:44 -0700

    saved

--- tests.py
+++ tests.py
@@ -591,7 +591,7 @@ def test_llama_decoder_layer():
 	print(hf_model.model.layers[0])
 	for hf_layer, tg_layer in zip(hf_model.model.layers, tg_model.model.layers):
 		hidden_states = make_test_data(4, 4)
-		_test_hf_reimplementation([], {}, hf_layer, "__call__", tg_layer, "__call__")
+		_test_hf_reimplementation([hidden_states], {}, hf_layer, "__call__", tg_layer, "__call__")
 	
 
 @tinygrad.Tensor.train(mode = False)
