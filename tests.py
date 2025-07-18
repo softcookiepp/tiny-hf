05f06df97e2f2b69de4480e8bd2b0cc72b645836
softcookiepp <softcookiepp@mailfence.com>
2025-07-18 14:33:38 -0700

    whyyyyyyyyyy

--- tests.py
+++ tests.py
@@ -609,7 +609,7 @@ def test_load_wan_models():
 	
 	tg_text_encoder = tg_text_encoder_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="text_encoder", torch_dtype=tg_adapter.float32)
 	tg_vae = tg_vae_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="vae", torch_dtype=tg_adapter.float32)
-	tg_transformer = tg_model_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="transformer", torch_dtype=tg_adapter.float32, low_cpu_mem_usage = True)
+	tg_transformer = tg_model_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="transformer")
 	
 	from transformers import UMT5EncoderModel as hf_text_encoder_class
 	from diffusers import AutoencoderKLWan as hf_vae_class
@@ -617,7 +617,7 @@ def test_load_wan_models():
 	
 	hf_text_encoder = hf_text_encoder_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="text_encoder", torch_dtype=torch.float32)
 	hf_vae = hf_vae_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="vae", torch_dtype=torch.float32)
-	hf_transformer = hf_model_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="transformer", torch_dtype=torch.float32, low_cpu_mem_usage = True)
+	hf_transformer = hf_model_class.from_pretrained("Wan-AI/Wan2.1-T2V-14B-Diffusers", subfolder="transformer")
 	
 	compare_state_dicts(hf_text_encoder, tg_text_encoder)
 	compare_state_dicts(hf_vae, tg_vae)
